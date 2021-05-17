fbf4a0ba4f015f71f94ce328f925e02e78ec19f9
ashish151090 <ashish151090@gmail.com>
2021-05-17 16:13:55 +0530

    changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 console.log('Client side javascript file is loaded!')
 
-fetch("http://localhost:3000/weather?address=agra").then((resp) => {
+fetch("/weather?address=agra").then((resp) => {
   console.log(resp)  
   resp.json().then((data)=>{
     console.log(data)
